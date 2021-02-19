29a18722500f0330b57ed5fb9689fa62bd1cbc45
Schuyler <schuyler.lindberg@ubc.ca>
2021-02-19 12:42:25 -0800

    add comments, make more readable

--- crawl-cluster.js
+++ crawl-cluster.js
@@ -1,23 +1,26 @@
 const { Cluster } = require('puppeteer-cluster');
 
-const fs = require('fs');
-const { url } = require('inspector');
-const crawlPath = 'https://www.library.ubc.ca';
-const outputPath = __dirname + "/output.json";
+const startUrl = 'https://www.library.ubc.ca';
+
+// const fs = require('fs');
+// const outputPath = __dirname + "/output.json";
 
 const dataObj = {};
 maxDepth = 2;
 
 (async () => {
+  // setup cluster
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: 5,
+    maxConcurrency: 5,  // max 5 instances at a time
   });
 
   const getLinks = async ({page, data}) => {
     const { url, depth } = data;
-    if (depth > maxDepth) { return; }
+    if (depth > maxDepth) { return; } // don't navigate deeper than maxDepth
     console.log(url, depth);
+
+    // go to page
     await page.goto(url);
 
     // does it contain repository?
@@ -26,17 +29,19 @@ maxDepth = 2;
       console.log('Page mentions a RePoSiTorY! --- DRINK!');
     }
 
+    // get links from page
     const links = await page.$$eval('a', a => a.map(l => l.href));
-    // console.log(links)
-
+    
+    // if link includes the startPath, crawl it (add to cluster queue)
     links.forEach(l => {
-      if (l.includes(crawlPath)) {
-        cluster.queue({url: l, depth: depth + 1}, getLinks)
+      if (link.includes(startUrl)) {
+        cluster.queue({url: link, depth: depth + 1}, getLinks)
       }
     })
   }
   
-  cluster.queue({url: crawlPath, depth: 0}, getLinks);
+  // start by queueing crawlPath (base domain)
+  cluster.queue({url: startUrl, depth: 0}, getLinks);
 
   await cluster.idle();
   await cluster.close();
